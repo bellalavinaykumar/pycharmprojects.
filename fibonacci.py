959c081c93acbdcfe16afe2d25f508dcefe8850a
vinay kumar bellala <vinaykumar24410@gmail.com>
2022-02-17 12:07:29 +0530

    changed add in fibonacci.

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,14 @@
-
 def fib(n):
-    a=0
-    b=1
-    print(a)
-    print(b)
+    a = 0
+    b = 1
+    if n==0 or n==1:
+        print(0)
+    else:
+        print(a)
+        print(b)
     for i in range(2,n+1):
         c=a+b
         a=b
         b=c
         print(c)
-
-print(fib(5))
+fib(4)
