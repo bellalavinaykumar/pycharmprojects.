ecf804426752bef6276c9144f3d9f548603e6ea8
vinay kumar bellala <vinaykumar24410@gmail.com>
2022-02-18 15:49:19 +0530

    Binary Search is updated

---
+++ Binary Search.py
@@ -0,0 +1,21 @@
+list=[2,5,9,11,16,21,25]
+pos=-1
+n=11
+def binary_search(list,n):
+    l=0
+    u=len(list)-1
+    while l<=u:
+        mid = (l + u) // 2
+        if list[mid]==n:
+            globals()['pos']=mid
+            return True
+        else:
+            if list[mid]<n:
+                l=mid+1
+            else:
+                u=mid+1
+    return False
+if binary_search(list,n):
+    print("found at",pos)
+else:
+    print("not found")
