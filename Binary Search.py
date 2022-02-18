f9d039bd13732c3a0c2a74be8bdf67b0c7d6c9be
vinay kumar bellala <vinaykumar24410@gmail.com>
2022-02-18 15:52:44 +0530

    Binary Search is changed from while loop too for loop

--- Binary Search.py
+++ Binary Search.py
@@ -1,10 +1,10 @@
 list=[2,5,9,11,16,21,25]
 pos=-1
-n=11
+n=25
 def binary_search(list,n):
     l=0
     u=len(list)-1
-    while l<=u:
+    for i in range(len(list)):
         mid = (l + u) // 2
         if list[mid]==n:
             globals()['pos']=mid
@@ -13,7 +13,7 @@ def binary_search(list,n):
             if list[mid]<n:
                 l=mid+1
             else:
-                u=mid+1
+                u=mid-1
     return False
 if binary_search(list,n):
     print("found at",pos)
